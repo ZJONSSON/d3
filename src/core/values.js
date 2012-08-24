e0784da1eedc157d2b62f6d6bd8671379099c4e3
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2012-08-24 12:33:17 -0400

    Add __key__ to elements in d3.values
    Retain key information as metadata when fetching d3.values

--- src/core/values.js
+++ src/core/values.js
@@ -1,5 +1,8 @@
 d3.values = function(map) {
   var values = [];
-  for (var key in map) values.push(map[key]);
+  for (var key in map) {
+  	map[key].__key__ = key
+  	values.push(map[key]);
+  };
   return values;
 };
